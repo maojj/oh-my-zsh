6c46111b1fff7ceedb3446b74934e6dadd957dbe
maojj <maojj@kanyun.com>
2025-08-13 18:10:38 +0800

    add: cc

--- plugins/maojj/maojj.plugin.zsh
+++ plugins/maojj/maojj.plugin.zsh
@@ -21,8 +21,7 @@ alias aaa='./scripts/wmk -wd wk-wasm-app-by-brotli'
 alias makeTest='cmake --build cmake-build-debug --target wk-util-test wk-render-test wk-editor-render-test wk-document-test wk-handler-test'
 alias fixup='gcmsg fixup;push'
 alias pre='pnpm export-import'
-alias kill3000='lsof -ti:3000 -sTCP:LISTEN | xargs -r kill -9'
-alias cc='claude --dangerously-skip-permissions'
+alias ccc='claude --dangerously-skip-permissions'
 
 function deleteLocalBranchNoInRemote() {  
   git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
