d76b601557add641819cd5ee86ee25f16cd861aa
maojj <maojj@kanyun.com>
2026-07-01 09:46:21 +0800

    ccc = codex

--- plugins/maojj/maojj.plugin.zsh
+++ plugins/maojj/maojj.plugin.zsh
@@ -21,7 +21,8 @@ alias aaa='./scripts/wmk -wd wk-wasm-app-by-brotli'
 alias makeTest='cmake --build cmake-build-debug --target wk-util-test wk-render-test wk-editor-render-test wk-document-test wk-handler-test'
 alias fixup='gcmsg fixup;push'
 alias pre='pnpm export-import'
-alias ccc='claude --dangerously-skip-permissions'
+alias ccc='codex'
+alias cccc='codex'
 
 function deleteLocalBranchNoInRemote() {  
   git fetch -p && git branch -vv | awk '/: gone]/{print $1}' | xargs git branch -D
